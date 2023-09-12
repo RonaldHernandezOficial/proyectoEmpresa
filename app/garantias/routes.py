37b9d9126e68fd56dc19a8b8c57bfa787535b8bf
Leidy <leidisilla3@gmail.com>
2023-09-12 09:11:40 -0500

    CRUD sin actualizar de pqrs

--- app/garantias/routes.py
+++ app/garantias/routes.py
@@ -40,3 +40,37 @@ def agregar_garantia():
         mysql.connection.commit()
         flash('!Garantía agregada satisfactoriamente¡')
     return redirect(url_for('modelo_garantias.insertar'))
+
+
+@modelo_garantias.route('/editar_garantia/<id>')
+def obtener_garantia(id):
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM garantia WHERE idGarantia = %s', (id))
+    dataG = cur.fetchall()
+    return render_template('editar_garantias.html', garantia = dataG[0])
+
+@modelo_garantias.route('/actualizar_garantia/<id>', methods = ['POST'])
+def actualizar_garantia(id):
+    if request.method == 'POST':
+        fechaGarantia = request.form['fechaGarantia']
+        descripcionGarantia = request.form['descripcionGarantia']
+        tipoGarantia = request.form['garantia']
+        cur = mysql.connection.cursor()
+        cur.execute("""
+            UPDATE garantia 
+            SET fechaGarantia = %s,
+                    descripcionGarantia = %s,
+                    tipoGarantia = %s
+            WHERE idGarantia = %s
+        """, (fechaGarantia, descripcionGarantia, tipoGarantia, id))
+        flash('Garantía actualizada satisfactoriamente')
+        cur.connection.commit()
+        return redirect(url_for('modelo_garantias.insertar'))
+
+@modelo_garantias.route('/eliminar_garantia/<string:id>')
+def eliminar_garantia(id):
+    cur = mysql.connection.cursor()  
+    cur.execute('DELETE FROM garantia WHERE idGarantia = {0}'.format(id))
+    mysql.connection.commit() 
+    flash('Garantía eliminada satisfactoriamente')
+    return redirect(url_for('modelo_garantias.insertar'))
--- app/serviciocliente/routes.py
+++ app/serviciocliente/routes.py
@@ -17,18 +17,34 @@ app.secret_key = 'mysecretkey'
 
 mysql = MySQL(app)
 
-@modelo_servicio.route("/reseñas")
-def reseña():
+@modelo_servicio.route("/pqr")
+def inicio():
     return render_template("serviciocliente.html")
+
+@modelo_servicio.route('/insertar')
+def insertar():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM pqrs')
+    data = cur.fetchall()   
+    return render_template('registrarpqr.html' , servicios = data)
 
 @modelo_servicio.route('/insertar', methods=['GET', 'POST'])
 def agregar_reseña():
     if request.method == 'POST':
         tipoPqrs = request.form['tipoPqrs']
         descripcionPqrs = request.form['descripcionPqrs']
-        cur = mysql.connection.curso()
+        cur = mysql.connection.cursor()
         cur.execute('INSERT INTO  pqrs (tipoPqrs, descripcionPqrs) VALUES (%s,%s)',
                     (tipoPqrs, descripcionPqrs))
         mysql.connection.commit()
         flash("¡PQR'S registrado exitosamente!")
+    return redirect(url_for('modelo_servicio.insertar'))
+
+
+@modelo_servicio.route('/eliminar_servicio/<string:id>')
+def eliminar_servicio(id):
+    cur = mysql.connection.cursor()  
+    cur.execute('DELETE FROM pqrs WHERE idPqrs = {0}'.format(id))
+    mysql.connection.commit() 
+    flash('Pqrs eliminado satisfactoriamente')
     return redirect(url_for('modelo_servicio.insertar'))

2ca5b3e5d1bc42bd1521c39a0e59bd4b893d7f06
RonaldHernandezOficial <santicraksito777@gmail.com>
2026-06-01 21:06:58 -0600

    Add sitemap

--- main.py
+++ main.py
@@ -9,33 +9,6 @@ cache = Cache(config={'CACHE_TYPE': 'SimpleCache'})
 # Creación de la aplicación Flask
 application = Flask(__name__)
 
-# Configuración de MySQL
-application.config['MYSQL_HOST'] = os.environ.get("MYSQL_HOST")
-application.config['MYSQL_USER'] = os.environ.get("MYSQL_USER")
-application.config['MYSQL_PASSWORD'] = os.environ.get("MYSQL_PASSWORD")  # ¡Cuidado! No deberías tener contraseña vacía en producción
-application.config['MYSQL_DB'] = os.environ.get("post_sale")
-application.secret_key = os.environ.get("s8f7as8f7as8f7a8s7f8as7f8as7")  # Deberías usar una clave más segura en producción
-
-# Inicialización de MySQL
-mysql = MySQL(application)
-
-# Importación y registro de blueprints
-from app.menu import modelo_menu
-application.register_blueprint(modelo_menu)
-
-@application.route('/')
-def redirigir_a_menu():
-    return redirect(url_for('modelo_menu.menu'))
-
-@application.route('/server-status')
-def server_status():
-    return {
-        "cpu": f"{psutil.cpu_percent()}%",
-        "ram": f"{psutil.virtual_memory().percent}%"
-    }
-
-
-
 @application.route('/sitemap.xml')
 def sitemap():
     xml = """<?xml version="1.0" encoding="UTF-8"?>
@@ -68,6 +41,32 @@ Allow: /
 
 Sitemap: https://www.fabriautomaticassas.com/sitemap.xml
 """, 200, {'Content-Type': 'text/plain'}
+
+
+# Configuración de MySQL
+application.config['MYSQL_HOST'] = os.environ.get("MYSQL_HOST")
+application.config['MYSQL_USER'] = os.environ.get("MYSQL_USER")
+application.config['MYSQL_PASSWORD'] = os.environ.get("MYSQL_PASSWORD")  # ¡Cuidado! No deberías tener contraseña vacía en producción
+application.config['MYSQL_DB'] = os.environ.get("post_sale")
+application.secret_key = os.environ.get("s8f7as8f7as8f7a8s7f8as7f8as7")  # Deberías usar una clave más segura en producción
+
+# Inicialización de MySQL
+mysql = MySQL(application)
+
+# Importación y registro de blueprints
+from app.menu import modelo_menu
+application.register_blueprint(modelo_menu)
+
+@application.route('/')
+def redirigir_a_menu():
+    return redirect(url_for('modelo_menu.menu'))
+
+@application.route('/server-status')
+def server_status():
+    return {
+        "cpu": f"{psutil.cpu_percent()}%",
+        "ram": f"{psutil.virtual_memory().percent}%"
+    }
 
 if __name__ == "__main__":
     application.run()
